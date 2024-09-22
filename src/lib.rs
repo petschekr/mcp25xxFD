70a6b1e2e3b792d9b9ac712c7eea7f2868c13205
Ryan Petschek <petschekr@gmail.com>
2024-09-22 14:17:11 -0400

    Implement SPI error handling

--- src/lib.rs
+++ src/lib.rs
@@ -25,33 +25,32 @@ impl<SPI: SpiDevice, Input: InputPin> MCP25xxFD<SPI, Input> {
         }
     }
 
-    pub async fn reset(&mut self) -> Result<(), ()> {
+    /// Resets the controller and places it back into Configuration Mode
+    pub async fn reset(&mut self) -> Result<(), SPI::Error> {
         let tx = Instruction::Reset.header(0x00);
-        self.spi.write(&tx).await.unwrap();
-
-        Ok(())
+        self.spi.write(&tx).await
     }
 
     /// Read a single register
-    pub async fn read_register<R: Register>(&mut self) -> Result<R, ()> {
+    pub async fn read_register<R: Register>(&mut self) -> Result<R, SPI::Error> {
         let tx = Instruction::Read.header(R::ADDRESS);
         let mut rx = [0u8; 6];
-        self.spi.transfer(&mut rx, &tx).await.unwrap(); // TODO: SPI error handling
+        self.spi.transfer(&mut rx, &tx).await?;
 
         Ok(R::parse(&rx[2..]))
     }
 
     /// Write a single register
-    pub async fn write_register<R: Register>(&mut self, register: R) -> Result<(), ()> {
+    pub async fn write_register<R: Register>(&mut self, register: R) -> Result<(), SPI::Error> {
         self.spi.transaction(&mut [
             Operation::Write(&Instruction::Write.header(R::ADDRESS)),
             Operation::Write(&R::serialize(register)),
-        ]).await.unwrap(); // TODO: SPI error handling
+        ]).await?;
 
         Ok(())
     }
 
-    pub async fn read_bytes<const B: usize>(&mut self, address: u16) -> Result<[u8; B], ()> {
+    pub async fn read_bytes<const B: usize>(&mut self, address: u16) -> Result<[u8; B], SPI::Error> {
         assert_eq!(B % 4, 0, "Must read in multiples of 4 data bytes");
         let tx = Instruction::Read.header(RAM_START + address);
         let mut rx = [0u8; B];
@@ -59,24 +58,24 @@ impl<SPI: SpiDevice, Input: InputPin> MCP25xxFD<SPI, Input> {
         self.spi.transaction(&mut [
             Operation::Write(&tx),
             Operation::Read(&mut rx),
-        ]).await.unwrap(); // TODO: SPI error handling
+        ]).await?;
 
         Ok(rx)
     }
 
-    pub async fn write_bytes(&mut self, address: u16, data: &[u8]) -> Result<(), ()> {
+    pub async fn write_bytes(&mut self, address: u16, data: &[u8]) -> Result<(), SPI::Error> {
         assert_eq!(data.len() % 4, 0, "Must write in multiples of 4 data bytes");
         let tx = Instruction::Write.header(RAM_START + address);
 
         self.spi.transaction(&mut [
             Operation::Write(&tx),
             Operation::Write(data),
-        ]).await.unwrap(); // TODO: SPI error handling
+        ]).await?;
 
         Ok(())
     }
 
-    pub async fn initialize_ram(&mut self, data: u8) -> Result<(), ()> {
+    pub async fn initialize_ram(&mut self, data: u8) -> Result<(), SPI::Error> {
         const INIT_INCREMENT: usize = 64; // Write 64 bytes at a time
         let bytes = [data; INIT_INCREMENT];
 
